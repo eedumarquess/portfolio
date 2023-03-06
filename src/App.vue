0ed8ce0e9e598408671983a29b0938bb62bda046
Eduardo Marques <edu@MacBook-Air-de-Eduardo.local>
2023-03-06 05:35:05 -0800

    first base commit with vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import Menu from './components/menu/Menu.vue';
+</script>
+
+<template>
+  <Menu />
+</template>
+
+<style>
+  @font-face {
+    font-family: "LeagueSpartan";
+    src: url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500&display=swap');;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    background: transparent;
+  }
+</style>
---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="menubar">
+    <ul>
+      <li>
+        HOME
+      </li>
+
+      <li>
+        ABOUT ME
+      </li>
+
+      <li>
+        PROJECTS
+      </li>
+
+      <li>
+        CONTACT ME
+      </li>
+    </ul>
+
+    <div class="icons">
+      <p>
+        icons
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .menubar {
+    font-family: "LeagueSpartan", Helvetica, Arial;
+    background-color: #222831;
+    color: #fff;
+    width: 45vw;
+    height: 100vh;
+  }
+
+  li{
+    font-size: 32px;
+    list-style: none;
+  }
+
+  li::before{
+    content: "\2192";
+  }
+</style>
