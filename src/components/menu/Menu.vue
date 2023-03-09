e95a76dcb4fe37a56f5dc3d1b6002c0bd49866d4
Eduardo Marques <edu@MacBook-Air-de-Eduardo.local>
2023-03-09 13:43:12 -0800

    Adjust menu component

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,29 +1,123 @@
-<script setup lang="ts">
+<script lang="ts">
 
+export default {
+  data() {
+    return {
+      isMenuActive: false,
+    };
+  },
+  methods: {
+    handleMenu() {
+      this.isMenuActive = !this.isMenuActive
+    },
+  }
+}
 </script>
 
 <template>
   <div class="menubar-sidebar">
     <img 
+      v-if="isMenuActive"
+      class="icon-menu" 
+      src="../../assets/images/xmark-solid.svg" 
+      @click="handleMenu"
+    />
+    <img 
+      v-else="isMenuActive"
       class="icon-menu" 
       src="../../assets/images/bars-solid.svg" 
-      v-on:click=""
+      @click="handleMenu"
     />
-  </div>
 
-  <div>
-
+    <nav
+      class="menubar-nav" 
+      :style="{
+        display: isMenuActive ? 'flex' : 'none',
+      }"
+    >
+      <div class="menubar-links">
+        <router-link to="/">
+          Home
+        </router-link>
+        <router-link to="/about-me">
+          About me
+        </router-link>
+        <router-link to="/projects">
+          Projects
+        </router-link>
+        <router-link to="/contact-me">
+          Contact Me
+        </router-link>
+      </div>
+      
+      <div class="menubar-icons">
+        <a href="">
+          <img src="../../assets/icons/linkedin.png" />
+        </a>
+        
+        <a href="">
+          <img src="../../assets/icons/github.png" />
+        </a>
+        
+        <a href="">
+          <img src="../../assets/icons/instagram.png" />
+        </a>
+        
+        <a href="">
+          <img src="../../assets/icons/twitter.png" />
+        </a>
+      </div>
+    </nav>
   </div>
 </template>
 
 <style>
   .menubar-sidebar {
-    position: relative;
+    position: fixed;
   }
 
   .icon-menu {
-    margin: 50px;
+    margin: 35px;
     filter: invert(95%) sepia(100%) saturate(14%) hue-rotate(213deg) brightness(104%) contrast(104%);
-    width: 60px;
+    width: 40px;
+    aspect-ratio: 1;
+    object-fit: contain;
+  }
+
+  .menubar-nav {
+    background-color: #222831;
+    width: 100vw;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .menubar-links {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .menubar-links > a {
+    padding: 5px;
+    margin: 5px 30px;
+    font-size: 45px;
+  }
+
+  .menubar-links > a::before {
+    content: "\2192";
+  }
+
+  .menubar-icons {
+    position: fixed;
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    bottom: 0;
+  }
+
+  .menubar-icons > a > img {
+    margin: 10px 7.5px;
+    width: 25px;
   }
 </style>
